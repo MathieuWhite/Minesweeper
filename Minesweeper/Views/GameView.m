51abca705f8c7b7e3c72966bbdafd22e7187ec46
Mathieu <mathieuwhite@me.com>
2014-10-04 16:26:35 -0400

    Cleaned up

--- Minesweeper/Views/GameView.m
+++ Minesweeper/Views/GameView.m
@@ -12,8 +12,6 @@
 @interface GameView() <UIScrollViewDelegate>
 
 @property (nonatomic, weak) UIScrollView *scrollView;
-//@property (nonatomic, weak) UIView *mineFieldView;
-//@property (nonatomic, weak) TileView *tileView;
 
 @property (nonatomic, weak) MinefieldView *minefieldView;
 
@@ -42,10 +40,6 @@
     // Set the background color
     [self setBackgroundColor: [UIColor colorWithRed: 46.0f/255.0f green: 46.0f/255.0f blue: 59.0f/255.0f alpha: 1.0f]];
     
-    // Create the minefield view
-    // Frame will depend on difficulty - to implement later..
-    //UIView *mineFieldView = [[UIView alloc] initWithFrame: CGRectMake(0, 0, 576, 576)];
-    
     MinefieldView *minefieldView = [[MinefieldView alloc] initWithDifficulty: @"easy"];
     //[minefieldView storeTileViewAdjacentTiles];
     
@@ -59,43 +53,14 @@
     [scrollView setShowsVerticalScrollIndicator: NO];
     [scrollView setDelegate: self];
     
-    /*
-    // Create the CardViews
-    for (NSUInteger row = 0; row < 9; row++)
-    {
-        for (NSUInteger col = 0; col < 9; col++)
-        {
-            CGFloat offsetX = (row * 64);
-            CGFloat offsetY = (col * 64);
-            
-            TileView *tileView = [[TileView alloc] initWithFrame: CGRectMake(offsetX, offsetY, 64, 64)];
-            
-            if ((row + col) % 2 == 0)
-                [tileView setDarkerTone: YES];
-            else [tileView setDarkerTone: NO];
-            
-            //[scrollView addSubview: tileView];
-            [mineFieldView addSubview: tileView];
-        }
-    }
-     */
-    
-    // Create a tile
-    //TileView *tileView = [[TileView alloc] initWithFrame: CGRectMake(0, 0, 64, 64)];
-    //[tileView setCenter: [scrollView center]];
-    
     // Add the components to the scroll view
     [scrollView addSubview: minefieldView];
     
     // Add the scroll view to the view
     [self addSubview: scrollView];
-    //[self addSubview: mineFieldView];
-    //[self addSubview: minefieldView];
     
     // Set each component to a property
     [self setScrollView: scrollView];
-    //[self setTileView: tileView];
-    
     [self setMinefieldView: minefieldView];
 }
 
