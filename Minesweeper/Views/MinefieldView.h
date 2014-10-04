9db8ed8a0a9e83753c91471cf39ccc5c6146e791
Mathieu <mathieuwhite@me.com>
2014-10-04 07:12:10 -0400

    Created MinefieldView

---
+++ Minesweeper/Views/MinefieldView.h
@@ -0,0 +1,13 @@
+//
+//  MinefieldView.h
+//  Minesweeper
+//
+//  Created by Mathieu White on 2014-10-04.
+//  Copyright (c) 2014 Mathieu White. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MinefieldView : UIView
+
+@end
---
+++ Minesweeper/Views/MinefieldView.m
@@ -0,0 +1,21 @@
+//
+//  MinefieldView.m
+//  Minesweeper
+//
+//  Created by Mathieu White on 2014-10-04.
+//  Copyright (c) 2014 Mathieu White. All rights reserved.
+//
+
+#import "MinefieldView.h"
+
+@implementation MinefieldView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
